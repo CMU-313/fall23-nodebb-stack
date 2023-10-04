c9cb5a375f62b4dc7c11cf42cc65a55cf36a444f
Tze Hng Loke <tloke@andrew.cmu.edu>
2023-10-04 15:26:14 -0400

    added a way to toggle endorsements as well as unendorse

--- src/posts/endorsements.ts
+++ src/posts/endorsements.ts
@@ -9,3 +9,16 @@ interface myPostObject extends PostObject{
     editedISO?: string;
 }
 
+export = function (Posts : myPostObject) {
+    async function toggleVote(type, pid, uid) {
+        const voteStatus = await Posts.endorsements;
+        await unendorse(pid, uid, type, voteStatus);
+        return await endorse(type, false, pid, uid, voteStatus);
+    }
+
+    async function unendorse(pid, uid, type, voteStatus) {
+        const owner = await Posts.pid;
+        return await endorse(voteStatus.upvoted ? 'downvote' : 'upvote', true, pid, uid, voteStatus);
+    }
+
+};
