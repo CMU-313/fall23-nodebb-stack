70e06bfe488a95fdb4bf60f2a54fbe3ba1aca5f3
Tze Hng Loke <tloke@andrew.cmu.edu>
2023-10-04 15:26:38 -0400

    added a way to endorse for the endorsement feature as well

--- src/posts/endorsements.ts
+++ src/posts/endorsements.ts
@@ -10,7 +10,7 @@ interface myPostObject extends PostObject{
 }
 
 export = function (Posts : myPostObject) {
-    async function toggleVote(type, pid, uid) {
+    async function toggleEndorsement(type, pid, uid) {
         const voteStatus = await Posts.endorsements;
         await unendorse(pid, uid, type, voteStatus);
         return await endorse(type, false, pid, uid, voteStatus);
@@ -21,4 +21,17 @@ export = function (Posts : myPostObject) {
         return await endorse(voteStatus.upvoted ? 'downvote' : 'upvote', true, pid, uid, voteStatus);
     }
 
+    async function endorse(type, unvote, pid, uid, voteStatus) {
+        uid = parseInt(uid, 10);
+        if (uid <= 0) {
+            throw new Error('[[error:not-logged-in]]');
+        }
+        const now = Date.now();
+
+        if (type === 'upvote' || unvote) {
+            await db.sortedSetRemove(`uid:${uid}:endorsements`, pid);
+        } else {
+            await db.sortedSetAdd(`uid:${uid}:endorsements`, now, pid);
+        }
+    }
 };
