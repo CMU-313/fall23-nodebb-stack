0fda2a6de48d8069fe1fb064c88cbd1edd420b17
Tze Hng Loke <tloke@andrew.cmu.edu>
2023-10-06 22:00:53 -0400

    Added function to update endorsement vote count

--- src/posts/endorsements.ts
+++ src/posts/endorsements.ts
@@ -2,6 +2,7 @@
 
 import * as db from '../database';
 import { PostObject } from '../types';
+const topics = require('../topics');
 
 
 interface myPostObject extends PostObject{
@@ -33,5 +34,32 @@ export = function (Posts : myPostObject) {
         } else {
             await db.sortedSetAdd(`uid:${uid}:endorsements`, now, pid);
         }
+    }
+
+    async function updateEndorsementVoteCount(postData) {
+        const topicData = await topics.getTopicFields(postData.tid, ['mainPid', 'cid', 'pinned']);
+
+        if (postData.uid) {
+            if (postData.votes !== 0) {
+                await db.sortedSetAdd(`cid:${topicData.cid}:uid:${postData.uid}:pids:votes`, postData.votes, postData.pid);
+            } else {
+                await db.sortedSetRemove(`cid:${topicData.cid}:uid:${postData.uid}:pids:votes`, postData.pid);
+            }
+        }
+
+        if (parseInt(topicData.mainPid, 10) !== parseInt(postData.pid, 10)) {
+            return await db.sortedSetAdd(`tid:${postData.tid}:posts:votes`, postData.votes, postData.pid);
+        }
+        const promises = [
+            topics.setTopicFields(postData.tid, {
+                upvotes: postData.upvotes,
+                downvotes: postData.downvotes,
+            }),
+            db.sortedSetAdd('topics:endorsements', postData.votes, postData.tid),
+        ];
+        if (!topicData.pinned) {
+            promises.push(db.sortedSetAdd(`cid:${topicData.cid}:tids:endorsements`, postData.votes, postData.tid));
+        }
+        await Promise.all(promises);
     }
 };
