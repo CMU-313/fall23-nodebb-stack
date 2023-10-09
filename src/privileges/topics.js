818c0c69e99b6078660e21c3fe1145a428277593
kc12341 <kylechen@andrew.cmu.edu>
2023-10-08 21:41:50 -0400

    added instructor variable to endorse privileges

--- src/privileges/topics.js
+++ src/privileges/topics.js
@@ -31,14 +31,14 @@ privsTopics.get = async function (tid, uid) {
     const privData = _.zipObject(privs, userPrivileges);
     const isOwner = uid > 0 && uid === topicData.uid;
     const isAdminOrMod = isAdministrator || isModerator;
-    const isInstructor = (user.accounttype == 'instructor')
+    const isInstructor = (user.accounttype === 'instructor')
     const editable = isAdminOrMod;
     const deletable = (privData['topics:delete'] && (isOwner || isModerator)) || isAdministrator;
     const mayReply = privsTopics.canViewDeletedScheduled(topicData, {}, false, privData['topics:schedule']);
 
     return await plugins.hooks.fire('filter:privileges.topics.get', {
         'topics:reply': (privData['topics:reply'] && ((!topicData.locked && mayReply) || isModerator)) || isAdministrator,
-        'topics:endorse': privData['topics:endorse'] && isInstructor,
+        'topics:endorse': privData['topics:endorse'] || isInstructor,
         'topics:read': privData['topics:read'] || isAdministrator,
         'topics:schedule': privData['topics:schedule'] || isAdministrator,
         'topics:tag': privData['topics:tag'] || isAdministrator,
